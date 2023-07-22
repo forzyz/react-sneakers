f604f53bf3f2a176b1901800fe38d1a2aa26de34
Ruslan <zimpuytel@gmail.com>
2023-07-22 14:25:54 +0300

    card folder and heart svg fixed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const arr = [
+  { imageUrl: "/img/sneakers/1.jpg", title: "Nike Blazer Mid Suede", price: 100 },
+  { imageUrl: "/img/sneakers/2.jpg", title: "Nike Air Max 270", price: 120 },
+  { imageUrl: "/img/sneakers/3.jpg", title: "Nike Blazer Mid Suede", price: 105 },
+  { imageUrl: "/img/sneakers/4.jpg", title: "Puma X Aka Boku Future Rider", price: 110 },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,13 +24,11 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-
-          <Card />
-
-          <Card />
-
-          <Card />
+          {
+            arr.map((obj) => (
+              <Card imageUrl={obj.imageUrl} title={obj.title} price={obj.price} onClick={() => { console.log(obj); }} />
+            ))
+          }
         </div>
       </div>
     </div>
